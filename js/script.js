f8077a842f0a10e32d0af3baa3a850764e543def
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-08-17 09:35:41 +0530

    added files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function timer() {
+  setTimeout(() => {
+    setValue(10);
+    setTimeout(() => {
+      setValue(9);
+      setTimeout(() => {
+        setValue(8);
+        setTimeout(() => {
+          setValue(7);
+          setTimeout(() => {
+            setValue(6);
+            setTimeout(() => {
+              setValue(5);
+              setTimeout(() => {
+                setValue(4);
+                setTimeout(() => {
+                  setValue(3);
+                  setTimeout(() => {
+                    setValue(2);
+                    setTimeout(() => {
+                      setValue(1);
+                      setTimeout(() => {
+                        setValue(0);
+                        setTimeout(() => {
+                          setValue("Happy New Year");
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}
+
+function setValue(value) {
+  const mainContainer = document.querySelector(".main-container");
+  if (value === "Happy New Year") {
+    firework();
+    mainContainer.innerHTML = `<p class="message">${value}<p>`;
+    return;
+  }
+  mainContainer.innerHTML = `<p class="count">${value}<p>`;
+}
+
+timer();
